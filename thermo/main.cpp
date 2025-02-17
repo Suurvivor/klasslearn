26c9af9e3a7dd095bc454e448f4e6280ce2ee4b9
Suurvivor <fiiremage@gmail.com>
2025-02-17 11:57:58 +0100

    popraa zadania

--- thermo/main.cpp
+++ thermo/main.cpp
@@ -1,4 +1,28 @@
-#include "thermo.h"
+#include <iostream>
+
+using namespace std;
+
+class Thermo{
+    public:
+        Thermo(float t, unsigned int m, const string &l){
+            temp = t;
+            mode = m;
+            location = l;
+        }
+        ~Thermo(){
+            cout << "Thermostat in " << location << "has been destroed"<< endl;
+        }
+
+        void displayStatus(){
+            cout << "Temperature:" << temp << "/5\nMode: " << (mode == 0 ? "OFF" : (mode == 1 ? "COOLING" : "HEATING")) << "\nLocation: " << location << endl;
+        }
+
+    private:
+        float temp;
+        unsigned int mode;
+        string location;
+
+};
 
 
 int main(){
